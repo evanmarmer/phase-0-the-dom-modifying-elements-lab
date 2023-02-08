50c90059d8fbaa57cae8b63e293cbd72a60e2268
evanmarmer <evanmarmer@gmail.com>
2023-02-08 14:18:29 -0700

    DOM modifyin elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = 'Evan is the champion'
+
+
+//let newHeader = document.createElement('h1 id="Victory">"is the champion"</h1>)
